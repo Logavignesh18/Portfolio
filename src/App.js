b5c96d9248dc637f0b58d1a251b0f898564e7331
Hitheash <hithi2003@gmail.com>
2024-06-05 15:04:01 +0530

    skills js done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Skills from "./components/Skills/skills";
+import Intro from "./components/Intro/intro";
 import Navbar from "./components/NavBar/navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
+      <Skills/>
     </div>
   );
 }
